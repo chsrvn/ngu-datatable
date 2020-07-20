035d1a695ead50a23c72fcffadbc55093dd4fa30
Sravan <chsrvn@gmail.com>
2020-07-20 17:34:14 +0200

    Make header optional

--- projects/ngu-datatable/src/lib/ngu-datatable-column/ngu-datatable-column.component.ts
+++ projects/ngu-datatable/src/lib/ngu-datatable-column/ngu-datatable-column.component.ts
@@ -1,23 +1,30 @@
-import {AfterViewInit, Component, ContentChild} from "@angular/core";
-import {NguDatatableCell} from "../ngu-datatable-cell.directive";
-import {NguDatatableHeader} from "../ngu-datatable-header.directive";
+import {
+  AfterViewInit,
+  Component,
+  ContentChild,
+  Input,
+  OnInit
+} from "@angular/core";
+import { NguDatatableCell } from "../ngu-datatable-cell.directive";
+import { NguDatatableHeader } from "../ngu-datatable-header.directive";
 
 @Component({
   selector: "ngu-datatable-column",
-  templateUrl: "./ngu-datatable-column.component.html",
+  template: "",
   styleUrls: ["./ngu-datatable-column.component.scss"]
 })
-export class NguDatatableColumn implements AfterViewInit {
+export class NguDatatableColumn implements OnInit {
+  @Input("property") property: string;
   @ContentChild(NguDatatableCell, { static: false })
   nguDatatableCell: NguDatatableCell;
   @ContentChild(NguDatatableHeader, { static: false })
   nguDatatableHeader: NguDatatableHeader;
 
-  constructor() {}
-
-  ngAfterViewInit(): void {
-    console.log(this.nguDatatableCell);
-    console.log(this.nguDatatableHeader);
-    // this.nguDatatableCell.
+  ngOnInit(): void {
+    if (!this.property) {
+      throw new TypeError(
+        "'Property' attribute is required for ngu-datatable-column. Example: <ngu-datatable-column property=\"id\">"
+      );
+    }
   }
 }
